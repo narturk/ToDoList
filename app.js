f3cd254175ab3ab7952f8fbc89a1f9457cc7ea81
nturk <nart_turk@bmc.com>
2023-05-02 19:50:49 +0300

    last push - Finished

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
 const port = 3000
 
 const app = express()
 
-var items = []
+const items = ["Buy Food", "Cook Food", "Eat Food"]
+const workItems = []
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.set('view engine', 'ejs')
 
 app.get("/", function(req, res){
-  var today = new Date()
+  const day = date.getDate()
 
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-
-  var day = today.toLocaleDateString('en-US', options)
-
-  res.render("list", {kindOfDay: day, newListItems: items})
+  res.render("list", {listTitle: day, newListItems: items})
 })
 
 app.post("/", function(req, res){
-  var item = req.body.newItem
-  items.push(item)
+  const item = req.body.newItem
 
-  res.redirect("/")
+  if (req.body.list === "Work"){
+    workItems.push(item)
+    res.redirect("/work")
+  }else{
+    items.push(item)
+    res.redirect("/")
+  }
+})
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.get("/about", function(req, res){
+  res.render("about")
 })
 
 app.listen(port, function(){
